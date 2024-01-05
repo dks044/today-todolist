f2d1cca6707d9ce3fa195d82102fee61a4cdc1b6
AJN <buzz781@gmail.com>
2024-01-05 16:07:06 +0900

    feat: 할일추가하기 기능 구현

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -1,21 +1,22 @@
 import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
 import { MdAdd } from 'react-icons/md';
+import {useTodoDispatch, useTodoNextId} from '../contexts/TodoContext';
+
 
 const CircleButton = styled.button`
   background: #38d9a9;
   &:hover {
-    background: #63ddbe;
+    background: #63e6be;
   }
   &:active {
-    background: #20c955;
+    background: #20c997;
   }
-  
+
   z-index: 5;
   cursor: pointer;
   width: 80px;
   height: 80px;
-  display: block;
   align-items: center;
   justify-content: center;
   font-size: 60px;
@@ -27,10 +28,7 @@ const CircleButton = styled.button`
   border-radius: 50%;
   border: none;
   outline: none;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  
+
 
   transition: 0.125s all ease-in;
   ${props =>
@@ -78,15 +76,39 @@ const Input = styled.input`
 
 function TodoCreate() {
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState('');
+
+  const dispatch = useTodoDispatch();
+  const nextId = useTodoNextId();
 
   const onToggle = () => setOpen(!open);
+  const onChange = e => setValue(e.target.value);
+  const onSubmit = e => {
+    e.preventDefault(); // 새로고침 방지
+    dispatch({
+      type: 'CREATE',
+      todo: {
+        id: nextId.current,
+        text: value,
+        done: false
+      }
+    });
+    setValue('');
+    setOpen(false);
+    nextId.current += 1;
+  };
 
   return (
     <>
       {open && (
         <InsertFormPositioner>
-          <InsertForm>
-            <Input autoFocus placeholder="할 일을 입력 후, Enter 를 누르세요" />
+          <InsertForm onSubmit={onSubmit}>
+            <Input
+              autoFocus
+              placeholder="할 일을 입력 후, Enter 를 누르세요"
+              onChange={onChange}
+              value={value}
+            />
           </InsertForm>
         </InsertFormPositioner>
       )}
@@ -97,4 +119,4 @@ function TodoCreate() {
   );
 }
 
-export default TodoCreate;
+export default React.memo(TodoCreate);
